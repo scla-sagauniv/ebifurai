44ad7f4eb1984dabc9774783015b8adabbe68697
shunsuke-tamura <wwwxx4869@gmail.com>
2023-08-26 20:24:01 +0900

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,5 @@
 import AppProps from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Theme
-      accentColor="cyan"
-      grayColor="gray"
-      panelBackground="solid"
-      scaling="100%"
-      radius="full"
-    >
-      <Component {...pageProps} />
-    </Theme>
-  );
+  return <Component {...pageProps} />;
 }
