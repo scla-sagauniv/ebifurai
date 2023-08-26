510d93e2ed1ff978eb2111fef709e7373ea07ce9
shunsuke-tamura <wwwxx4869@gmail.com>
2023-08-26 18:35:07 +0900

    add libraly

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,15 @@
 import AppProps from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Theme
+      accentColor="cyan"
+      grayColor="gray"
+      panelBackground="solid"
+      scaling="100%"
+      radius="full"
+    >
+      <Component {...pageProps} />
+    </Theme>
+  );
 }
