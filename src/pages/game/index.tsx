d451322ffcbdb4de5c6b04797bc2e5b0febafe6b
shunsuke-tamura <wwwxx4869@gmail.com>
2023-08-27 06:52:43 +0900

    update: svgにできた

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,10 +1,20 @@
 import Map from "@/components/Game/map";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { data } from "./data";
+// @ts-ignore
+import geojson2svg from "geojson-to-svg";
+import SVG from "react-inlinesvg";
 
 export default function Game() {
   const render = (status: Status) => {
     return <h1>{status}</h1>;
   };
+
+  const svg = geojson2svg()
+    .styles({ Polygon: { fill: "none", stroke: "red" } })
+    .data(data)
+    .render()
+    .replace("/></g></svg>", 'stroke-width="0.01" /></g></svg>');
 
   return (
     <div
@@ -15,6 +25,7 @@ export default function Game() {
         padding: 0,
       }}
     >
+      <SVG src={svg} style={{ transform: "rotate(180deg)" }}></SVG>
       <Wrapper apiKey={process.env.NEXT_PUBLIC_MAP_API_KEY!} render={render}>
         <Map></Map>
       </Wrapper>
